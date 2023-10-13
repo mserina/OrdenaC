8f322fe43e0a4055a5b46292ae92196a92d7d159
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2023-10-13 12:50:29 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1;
+            float n2;
+            float n3;
+
+            Console.WriteLine("Primer numero");
+            n1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo numero");
+            n2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Tercer numero");
+            n3 = Single.Parse(Console.ReadLine());
+
+            if 
+        }
+    }
+}
